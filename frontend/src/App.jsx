3ff79f669d4fdeef07e99809107455ce8f8c1a77
Nikhil Reddy <s.nikhilreddy3446@gamil.com>
2025-09-09 23:57:03 +0530

    fix: somehting related to RegeisterPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,29 @@
 // in src/App.jsx
 
 import { Routes, Route } from 'react-router-dom';
+
+// --- THIS SECTION IS THE MOST LIKELY SOURCE OF THE ERROR ---
+// Are all three of these lines present and spelled correctly?
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import RegisterPage from './pages/RegisterPage'; // <-- This is the one the error is about
 import TodoPage from './pages/TodoPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
-      {/* A Route for the main page, which is our Todo list */}
-      <Route path="/" element={<TodoPage />} />
+      <Route 
+        path="/" 
+        element={
+          <ProtectedRoute>
+            <TodoPage />
+          </ProtectedRoute>
+        } 
+      />
       
-      {/* A Route for the login page */}
-      <Route path="/login" element={<LoginPage />} />
-
-      {/* A Route for the register page */}
+      {/* If the import is missing, this line will cause the "not defined" error */}
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
